82706ee3459c5cb281d98e7690dd2ff1b2a949c1
M4tthewDE <matthias.kronberg@gmail.com>
2025-02-10 17:20:09 +0100

    feat: invokespecial

--- internal/jvm.go
+++ internal/jvm.go
@@ -75,6 +75,7 @@ func (r *Runner) runMain(className string) error {
 }
 
 const GET_STATIC = 0xb2
+const INVOKE_SPECIAL = 0xb7
 const INVOKE_STATIC = 0xb8
 const NEW = 0xbb
 const DUP = 0x59
@@ -97,6 +98,9 @@ func (r *Runner) run(code []byte) error {
 		case DUP:
 			log.Println("executing dup")
 			err = r.dup()
+		case INVOKE_SPECIAL:
+			log.Println("executing invokespecial")
+			err = r.invokeSpecial(code)
 		default:
 			err = errors.New(
 				fmt.Sprintf("unknown instruction %x", instruction),
@@ -112,6 +116,74 @@ func (r *Runner) run(code []byte) error {
 		}
 
 	}
+}
+func (r *Runner) invokeSpecial(code []byte) error {
+	index := (uint16(code[r.pc+1])<<8 | uint16(code[r.pc+2]))
+	r.pc += 3
+
+	refInfo, err := r.currentClass.ConstantPool.Ref(index)
+	if err != nil {
+		return err
+	}
+
+	classInfo, err := r.currentClass.ConstantPool.Class(refInfo.ClassIndex)
+	if err != nil {
+		return err
+	}
+
+	className, err := r.currentClass.ConstantPool.GetUtf8(classInfo.NameIndex)
+	if err != nil {
+		return err
+	}
+
+	err = r.initializeClass(className)
+	if err != nil {
+		return err
+	}
+
+	c, err := r.loader.Load(className)
+	if err != nil {
+		return err
+	}
+
+	nameAndType, err := r.currentClass.ConstantPool.NameAndType(refInfo.NameAndTypeIndex)
+	if err != nil {
+		return err
+	}
+
+	methodName, err := r.currentClass.ConstantPool.GetUtf8(nameAndType.NameIndex)
+	if err != nil {
+		return err
+	}
+
+	method, ok, err := r.currentClass.GetMethod(methodName)
+	if err != nil {
+		return err
+	}
+
+	if !ok {
+		return errors.New("method not found")
+	}
+
+	descriptor, err := r.currentClass.ConstantPool.GetUtf8(nameAndType.DescriptorIndex)
+	if err != nil {
+		return err
+	}
+
+	methodDescriptor, err := class.NewMethodDescriptor(descriptor)
+	if err != nil {
+		return err
+	}
+
+	codeAttribute, err := method.CodeAttribute()
+	if err != nil {
+		return err
+	}
+
+	operands := r.stack.PopOperands(len(methodDescriptor.Parameters))
+
+	r.currentClass = c
+	return r.runMethod(codeAttribute.Code, methodName, operands)
 }
 
 func (r *Runner) dup() error {
@@ -135,10 +207,12 @@ func (r *Runner) new(code []byte) error {
 		return err
 	}
 
+	oldClass := r.currentClass
 	err = r.initializeClass(className)
 	if err != nil {
 		return err
 	}
+	r.currentClass = oldClass
 
 	c, err := r.loader.Load(className)
 	if err != nil {
